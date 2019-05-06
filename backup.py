91fccff591d75cf70aa0334d5408090035149331
CodedChops <devham01@gmail.com>
2019-05-06 22:00:42 +0100

    Editing pathnames to allow for other users to run program

--- backup.py
+++ backup.py
@@ -1,41 +1,43 @@
 from shutil import copyfile
 import os
 
-name = "***"
+cpu_name = "***" #computer's id path
+def_path = "C:\\Users\\%s\\" % cpu_name #default computer path
+backup_path = ""
 
 #Upgrade to work on any computer
 #When code is finished import into main.py and call every runtime
 
 def backup():
 	transfer = False
-	if not os.path.exists("C:\\Users\\%s\\Documents\\OrderRecorderBU" % name):
-		if os.path.isfile("C:\\Users\\%s\\Desktop\\Projects\\Python\\OrderRecorder\\orders.txt" % name) \
-		and os.path.isfile("C:\\Users\\%s\\Desktop\\Projects\\Python\\OrderRecorder\\references.txt" % name):
+	if not os.path.exists("%sDocuments\\OrderRecorderBU" % def_path):
+		if os.path.isfile("%sDesktop\\Projects\\Python\\OrderRecorder\\orders.txt" % def_path) \
+		and os.path.isfile("%sDesktop\\Projects\\Python\\OrderRecorder\\references.txt" % def_path):
 		
 			print("\nBackup Folder doesn't exist, creating new folder...")
-			os.mkdir("C:\\Users\\%s\\Documents\\OrderRecorderBU" % name)
+			os.mkdir("%sDocuments\\OrderRecorderBU" % def_path)
 			
 			print("\nTransferring data from order log to backup log...")
-			copyfile("C:\\Users\\%s\\Desktop\\Projects\\Python\\OrderRecorder\\orders.txt" % name, 
-				"C:\\Users\\%s\\Documents\\OrderRecorderBU\\orders.txt" % name)
-			copyfile("C:\\Users\\%s\\Desktop\\Projects\\Python\\OrderRecorder\\references.txt" % name, 
-				"C:\\Users\\%s\\Documents\\OrderRecorderBU\\references.txt" % name)
+			copyfile("%sDesktop\\Projects\\Python\\OrderRecorder\\orders.txt" % def_path, 
+				"%sDocuments\\OrderRecorderBU\\orders.txt" % def_path)
+			copyfile("%sDesktop\\Projects\\Python\\OrderRecorder\\references.txt" % def_path, 
+				"%sDocuments\\OrderRecorderBU\\references.txt" % def_path)
 			transfer = True
 		else:
 			print("\nOne or more files are missing!\n")
 	else:
-		if os.path.isfile("C:\\Users\\%s\\Desktop\\Projects\\Python\\OrderRecorder\\orders.txt" % name) \
-		and os.path.isfile("C:\\Users\\%s\\Desktop\\Projects\\Python\\OrderRecorder\\references.txt" % name):
-			with open("C:\\Users\\%s\\Documents\\OrderRecorderBU\\orders.txt" % name, "r") as a_log:
-				with open("C:\\Users\\%s\\Desktop\\Projects\\Python\\OrderRecorder\\orders.txt" % name, "r") as b_log:
+		if os.path.isfile("%sDesktop\\Projects\\Python\\OrderRecorder\\orders.txt" % def_path) \
+		and os.path.isfile("%sDesktop\\Projects\\Python\\OrderRecorder\\references.txt" % def_path):
+			with open("%sDocuments\\OrderRecorderBU\\orders.txt" % def_path, "r") as a_log:
+				with open("%sDesktop\\Projects\\Python\\OrderRecorder\\orders.txt" % def_path, "r") as b_log:
 					if len(a_log.read()) == 0 or len(a_log.read()) != len(b_log.read()):
-						copyfile("C:\\Users\\%s\\Desktop\\Projects\\Python\\OrderRecorder\\orders.txt" % name, 
-							"C:\\Users\\%s\\Documents\\OrderRecorderBU\\orders.txt" % name)
-			with open("C:\\Users\\%s\\Documents\\OrderRecorderBU\\references.txt" % name, "r") as c_log:
-				with open("C:\\Users\\%s\\Desktop\\Projects\\Python\\OrderRecorder\\references.txt" % name, "r") as d_log:
+						copyfile("%sDesktop\\Projects\\Python\\OrderRecorder\\orders.txt" % def_path, 
+							"sDocuments\\OrderRecorderBU\\orders.txt" % def_path)
+			with open("%sDocuments\\OrderRecorderBU\\references.txt" % def_path, "r") as c_log:
+				with open("%sDesktop\\Projects\\Python\\OrderRecorder\\references.txt" % def_path, "r") as d_log:
 					if len(c_log.read()) == 0 or len(c_log.read()) != len(d_log.read()):
-						copyfile("C:\\Users\\%s\\Desktop\\Projects\\Python\\OrderRecorder\\references.txt" % name, 
-							"C:\\Users\\%s\\Documents\\OrderRecorderBU\\references.txt" % name)
+						copyfile("%sDesktop\\Projects\\Python\\OrderRecorder\\references.txt" % def_path, 
+							"%sDocuments\\OrderRecorderBU\\references.txt" % def_path)
 		transfer = True
 	if transfer:
 		print("\nTransfer Complete! Program ended\n")
